defa17b89efe891cb04b6e850367cc1bb2f5acca
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-11-04 13:38:15 -0500

    Ensure docstrings are numpy and also do a bit of lint and format

--- arrays/completed/test_arrays_completed.py
+++ arrays/completed/test_arrays_completed.py
@@ -2,48 +2,57 @@ from arrays_completed import add_arrays, subtract_arrays, multiply_arrays, divid
 import pytest
 
 
-@pytest.mark.parametrize("a, b, expect",
-                         [([1, 2, 3], [4, 5, 6], [5, 7, 9]),
-                          ([1, 2, 3], [-1, -2, -3], [0, 0, 0]),
-                          ([1, 2, 3], [-10, -20, -30], [-9, -18, -27])
-                          ])
+@pytest.mark.parametrize(
+    "a, b, expect",
+    [
+        ([1, 2, 3], [4, 5, 6], [5, 7, 9]),
+        ([1, 2, 3], [-1, -2, -3], [0, 0, 0]),
+        ([1, 2, 3], [-10, -20, -30], [-9, -18, -27]),
+    ],
+)
 def test_add_arrays(a, b, expect):
-
     output = add_arrays(a, b)
 
     assert output == expect
 
 
-@pytest.mark.parametrize("a, b, expect",
-                         [([1, 2, 3], [4, 5, 6], [-3, -3, -3]),
-                          ([1, 2, 3], [1, 2, 3], [0, 0, 0]),
-                          ([1, 2, 3], [-10, -20, -30], [11, 22, 33])
-                          ])
+@pytest.mark.parametrize(
+    "a, b, expect",
+    [
+        ([1, 2, 3], [4, 5, 6], [-3, -3, -3]),
+        ([1, 2, 3], [1, 2, 3], [0, 0, 0]),
+        ([1, 2, 3], [-10, -20, -30], [11, 22, 33]),
+    ],
+)
 def test_subtract_arrays(a, b, expect):
     output = subtract_arrays(a, b)
 
     assert output == expect
 
 
-@pytest.mark.parametrize("a, b, expect",
-                         [([1, 2, 3], [4, 5, 6], [4, 10, 18]),
-                          ([1, 2, 3], [0, 0, 0], [0, 0, 0]),
-                          ([1, 2, 3], [-10, -20, -30], [-10, -40, -90])
-                          ])
+@pytest.mark.parametrize(
+    "a, b, expect",
+    [
+        ([1, 2, 3], [4, 5, 6], [4, 10, 18]),
+        ([1, 2, 3], [0, 0, 0], [0, 0, 0]),
+        ([1, 2, 3], [-10, -20, -30], [-10, -40, -90]),
+    ],
+)
 def test_multiply_arrays(a, b, expect):
-
     output = multiply_arrays(a, b)
 
     assert output == expect
 
 
-@pytest.mark.parametrize("a, b, expect",
-                         [([3, 2, 1], [4, 5, 4], [0.75, 0.4, 0.25]),
-                          ([0, 0, 0], [1, 2, 3], [0, 0, 0]),
-                          ([6, -4, 3], [2, 2, -1], [3, -2, -3])
-                          ])
+@pytest.mark.parametrize(
+    "a, b, expect",
+    [
+        ([3, 2, 1], [4, 5, 4], [0.75, 0.4, 0.25]),
+        ([0, 0, 0], [1, 2, 3], [0, 0, 0]),
+        ([6, -4, 3], [2, 2, -1], [3, -2, -3]),
+    ],
+)
 def test_divide_arrays(a, b, expect):
-
     output = divide_arrays(a, b)
 
     assert output == expect
